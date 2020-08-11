59a86080304bdecec7924f55708cda4d65daa911
Danny Lin <danny@kdrag0n.dev>
2020-08-10 20:05:31 -0700

    Update app title

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class PaintApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Touchpaint',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
