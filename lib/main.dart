134e9efcb2e43d7d01b1275748a6e9a03ec63052
Danny Lin <danny@kdrag0n.dev>
2020-08-11 00:30:14 -0700

    Add menus

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,197 @@ class PaintApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
+enum Mode {
+  PAINT,
+  FILL,
+  FOLLOW,
+  BLANK
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  Mode _mode = Mode.PAINT;
+  bool _showEventRate = false;
+  int _brushSize = 2;
+  int _paintClearDelay = 0;
+
+  Future<void> _changeMode() async {
+    Mode newMode = await showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+            title: const Text('Select mode'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: const Text('Paint'),
+                onPressed: () { Navigator.pop(context, Mode.PAINT); },
+              ),
+              SimpleDialogOption(
+                child: const Text('Fill'),
+                onPressed: () { Navigator.pop(context, Mode.FILL); },
+              ),
+              SimpleDialogOption(
+                child: const Text('Follow'),
+                onPressed: () { Navigator.pop(context, Mode.FOLLOW); },
+              ),
+              SimpleDialogOption(
+                child: const Text('Blank'),
+                onPressed: () { Navigator.pop(context, Mode.BLANK); },
+              ),
+            ],
+        ),
+    );
+
+    setState(() {
+      _mode = newMode;
+    });
+  }
+
+  Future<void> _changeBrushSize() async {
+    int newSize = await showDialog(
+        context: context,
+        builder: (context) => SimpleDialog(
+            title: const Text('Select brush size'),
+            children: <Widget>[
+              SimpleDialogOption(
+                child: const Text('1 physical pixel'),
+                onPressed: () { Navigator.pop(context, 1 / MediaQuery.of(context).devicePixelRatio); },
+              ),
+              SimpleDialogOption(
+                child: const Text('1 px'),
+                onPressed: () { Navigator.pop(context, 1); },
+              ),
+              SimpleDialogOption(
+                child: const Text('2 px'),
+                onPressed: () { Navigator.pop(context, 2); },
+              ),
+              SimpleDialogOption(
+                child: const Text('3 px'),
+                onPressed: () { Navigator.pop(context, 3); },
+              ),
+              SimpleDialogOption(
+                child: const Text('5 px'),
+                onPressed: () { Navigator.pop(context, 5); },
+              ),
+              SimpleDialogOption(
+                child: const Text('10 px'),
+                onPressed: () { Navigator.pop(context, 10); },
+              ),
+              SimpleDialogOption(
+                child: const Text('15 px'),
+                onPressed: () { Navigator.pop(context, 15); },
+              ),
+              SimpleDialogOption(
+                child: const Text('50 px'),
+                onPressed: () { Navigator.pop(context, 50); },
+              ),
+              SimpleDialogOption(
+                child: const Text('150 px'),
+                onPressed: () { Navigator.pop(context, 150); },
+              ),
+            ],
+        ),
+    );
+
+    setState(() {
+      _brushSize = newSize;
+    });
+  }
+
+  Future<void> _changeClearDelay() async {
+    int newDelay = await showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        title: const Text('Select paint clear delay'),
+        children: <Widget>[
+          SimpleDialogOption(
+            child: const Text('On next stroke'),
+            onPressed: () { Navigator.pop(context, 0); },
+          ),
+          SimpleDialogOption(
+            child: const Text('100 ms'),
+            onPressed: () { Navigator.pop(context, 100); },
+          ),
+          SimpleDialogOption(
+            child: const Text('250 ms'),
+            onPressed: () { Navigator.pop(context, 250); },
+          ),
+          SimpleDialogOption(
+            child: const Text('500 ms'),
+            onPressed: () { Navigator.pop(context, 500); },
+          ),
+          SimpleDialogOption(
+            child: const Text('1 second'),
+            onPressed: () { Navigator.pop(context, 1000); },
+          ),
+          SimpleDialogOption(
+            child: const Text('2 seconds'),
+            onPressed: () { Navigator.pop(context, 2000); },
+          ),
+          SimpleDialogOption(
+            child: const Text('5 seconds'),
+            onPressed: () { Navigator.pop(context, 5000); },
+          ),
+          SimpleDialogOption(
+            child: const Text('Never'),
+            onPressed: () { Navigator.pop(context, -1); },
+          ),
+        ],
+      ),
+    );
+
+    setState(() {
+      _paintClearDelay = newDelay;
+    });
+  }
+
+  void selectMenuItem(String item) {
+    switch (item) {
+      case 'Show event rate':
+        setState(() {
+          _showEventRate = !_showEventRate;
+        });
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Touchpaint'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.settings),
+            tooltip: 'Mode',
+            onPressed: _changeMode,
+          ),
+          IconButton(
+            icon: Icon(Icons.brush),
+            tooltip: 'Brush size',
+            onPressed: _changeBrushSize,
+          ),
+          IconButton(
+            icon: Icon(Icons.access_time),
+            tooltip: 'Clear delay',
+            onPressed: _changeClearDelay,
+          ),
+          PopupMenuButton<String>(
+            onSelected: selectMenuItem,
+            itemBuilder: (context) => <PopupMenuEntry<String>>[
+              CheckedPopupMenuItem<String>(
+                checked: _showEventRate,
+                value: 'Show event rate',
+                child: const Text('Show event rate'),
+              ),
+            ],
+          ),
+        ],
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
