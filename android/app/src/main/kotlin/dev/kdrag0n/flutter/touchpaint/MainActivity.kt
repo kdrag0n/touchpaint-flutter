38964bb6215b3fe6a5c289d596c1d129ad23d3bb
Danny Lin <danny@kdrag0n.dev>
2020-08-10 17:13:58 -0700

    Initialize Flutter project

---
+++ android/app/src/main/kotlin/dev/kdrag0n/flutter/touchpaint/MainActivity.kt
@@ -0,0 +1,12 @@
+package dev.kdrag0n.flutter.touchpaint
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
